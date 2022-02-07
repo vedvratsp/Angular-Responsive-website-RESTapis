1cf5cdd727ff7089e4ba11ce004d45b824be75a0
Vedvrat Singh Parihar <vedvratsp@gmail.com>
2022-02-07 14:03:11 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  memberName = 'Vedvrat';
+  l1: string = 'Bihari';
+  listOflanguages: string[] = ['English', 'Hindi', 'Marathi'];
+
+  addlanguages() {
+    console.log('clicked the function');
+
+    this.listOflanguages.push(this.l1);
+    console.log(this.listOflanguages);
+  }
+  deleteFirstlanguages() {
+    console.log('Clicked the delete button');
+    this.listOflanguages.splice(0 , 1);
+    console.log(this.listOflanguages);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PostListComponent,
+    PostComponent,
+    PostEditComponent,
+    AuthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
